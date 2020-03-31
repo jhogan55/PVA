f9abee0426161c6a65d2330cd1e73d4443ffc473
Jeremy Hogan <jhogan@ucalgary.ca>
2020-03-30 20:31:56 -0600

    Update VitalRates.cs

--- VitalRates.cs
+++ VitalRates.cs
@@ -8,10 +8,11 @@ namespace PVAfront
 {
     class VitalRates
     {
-        //Starting vital rates for Santa Rosa Capuchins, as of Feb 2020. THESE NEED RECALCULATING, CURRENTLY JUST DIVIDED OLD RATES BY 12  
-        public const double AMRRATE = 0.025; //Annual rate of 30% amr divided by 12 
+        //Starting vital rates for Santa Rosa Capuchins, as of Feb 2020. Mar 2020 these are calculated from monthly census records, 1986 to end of 2019
+        //Only groups BC, CP, CPAD, CPRM, EX, GN, LV and SE are included due to small samples/temporal gaps from other groups
+        public const double AMRRATE = 0.03; //Calculated monthly now 
         public const int RISKPERIOD = 6; //length a risk period lasts for after a takeover (6 months for now)
-        public const double SEXRATIO = 0.5; //ratio of males to females at birth. 
+        public const double SEXRATIO = 0.5; //ratio of males to females at birth. CAN THIS BE REFINED? 
 
         //NOTE: current juve and adult age "graduations" are set to include a 6 month pregnancy period as "infancy". Will likely want to change this in future iterations
         //The problem with this current format is that an infant in-utero has the same mortality risk as a live one, which is probably not true 
@@ -22,20 +23,20 @@ namespace PVAfront
         //public const int PREG = 6; //length of pregnancy. Not in use but will if infant/fetus are split (as they should be) 
 
         //Adult female survival and fecundity, mean and sd. 
-        double afSurvMean = 0.996;
-        double afSurvSd = 0.01;
+        double afSurvMean = 0.992665;
+        double afSurvSd = 0.04144;
         double afReprodMean = 0.042; //how do we decide this? Is it static or increases w/distance from last infant? 
         double afReprodSd = 0.005;
 
         //Juve female survival, mean and sd
-        double juvSurvMean = 0.996;
-        double juvSurvSd = 0.01;
+        double juvSurvMean = 0.996619;
+        double juvSurvSd = 0.026448;
         
         //Infant female survival, mean and sd, during stable and AMR periods 
-        double infSurvStbMean = 0.967;
-        double infSurvStbSd = 0.0167;
-        double infSurvAmrMean = 0.933;
-        double infSurvAmrSd = 0.025;
+        double infSurvStbMean = 0.969926;
+        double infSurvStbSd = 0.136259;
+        double infSurvAmrMean = 0.807388;
+        double infSurvAmrSd = 0.338586;
 
         //method to assign the correct mean survival based on 1) age class and 2) whether its AMR period 
         public double ReturnSurvMean(int i, bool a) 

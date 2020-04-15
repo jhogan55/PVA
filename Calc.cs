71e5b9923b28087078afc71f1727615e35cc8796
Jeremy Hogan <jhogan@ucalgary.ca>
2020-04-15 16:05:21 -0600

    Update Calc.cs
    Major change: a and b are now calculated from mean and sd BEFORE using the McCaffery sampling code.

--- Calc.cs
+++ Calc.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace PVAfront
 {
@@ -20,37 +21,37 @@ namespace PVAfront
             else return false; //random number > probability, event didnt happen 
         }
 
-        public double SampleBeta(double mean, double sd) //function for sampling from beta distribution 
+        public double SampleBeta(double mean, double sd) //function for sampling from beta distribution
         {
-            //taken from James McCaffrey, based on 1978 paper from RCH Cheng. 
-            //https://jamesmccaffrey.wordpress.com/2017/11/01/more-on-sampling-from-the-beta-distribution-using-c/cheng_ba_csharp/
-            // Assumption: mean, sd greater-than 0
 
-            double alpha = mean + sd;
+            //sampler taken from James McCaffrey, based on 1978 paper from RCH Cheng. 
+            //https://jamesmccaffrey.wordpress.com/2017/11/01/more-on-sampling-from-the-beta-distribution-using-c/cheng_ba_csharp/
+            //Add-on: mean and sd need to be converted to "a" and "b"  
+
+            double a = (((1 - mean) / (sd * sd)) - (1 / mean)) * (mean * mean);
+            double b = a * ((1 / mean) - 1);
+            double alpha = a + b;
             double beta = 0.0;
             double u1, u2, w, v = 0.0;
 
-            if (Math.Min(mean, sd) <= 1.0)
-                beta = Math.Max(1 / mean, 1 / sd);
+            if (Math.Min(a, b) <= 1.0)
+                beta = Math.Max(1 / a, 1 / b);
             else
-                beta = Math.Sqrt((alpha - 2.0) / (2 * mean * sd - alpha));
-            double gamma = mean + 1 / beta;
+                beta = Math.Sqrt((alpha - 2.0) / (2 * a * b - alpha));
+            double gamma = a + 1 / beta;
             while (true)
             { 
                 u1 = this.rnd.NextDouble();
                 u2 = this.rnd.NextDouble();
                 v = beta * Math.Log(u1 / (1 - u1));
-                w = mean * Math.Exp(v);
-                double tmp = Math.Log(alpha / (sd + w));
+                w = a * Math.Exp(v);
+                double tmp = Math.Log(alpha / (b + w));
                 if (alpha * tmp + (gamma * v) - 1.3862944 >= Math.Log(u1 * u1 * u2))
                     break;
             }
-            double x = w / (sd + w);
+            double x = w / (b + w);
             return x;
         } 
-
-
-
 
         //need to build a function that counts the number of individuals in the population, maybe per age class. Do that here or in different class? 
         int PopCount(List<simpleInd> popInd)
